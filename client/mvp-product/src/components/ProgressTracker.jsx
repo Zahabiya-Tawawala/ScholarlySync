666182cf0a82b17ea79091482e869517ab9046d3
Zahabiya Tawawala <zahabiyatawawala1@gmail.com>
2025-01-07 08:16:32 +0530

    daily commit

--- client/mvp-product/src/components/ProgressTracker.jsx
+++ client/mvp-product/src/components/ProgressTracker.jsx
@@ -4,6 +4,7 @@ const ProgressTracker = () => {
   return (
     <div>
       this is Progress Tracker
+      {/* here all the progress will be tracked need to check the backend if this fucntionality!!! */}
     </div>
   )
 }
